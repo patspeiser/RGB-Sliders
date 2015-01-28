5b0443fc9b3d12400931a3f2cec413c09d56321a
pat <pat@MacBook-Pro.home>
2015-01-27 20:08:13 -0500

    added sliders. got tired. will continue tomorrow

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +27,3 @@ class ViewController: UIViewController {
 
 }
 
-//thing 
-
-//test 2
