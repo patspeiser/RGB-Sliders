091ccd98af0abad2f7d6a37f361e94e00fbb8734
pat <pat@MacBook-Pro.home>
2015-01-28 21:34:18 -0500

    continuing work on view controllers and navigation

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -13,17 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        //view.backgroundColor = UIColor(red: red, green: green,  blue: blue,  alpha: 1)
+        
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    
+    }
 
 }
-
